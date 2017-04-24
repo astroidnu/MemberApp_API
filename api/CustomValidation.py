58b6abc19916ec13c0c8f04d723475677f25d4b1
Ibnu Muzzakkir <personalict@gmail.com>
2017-04-25 00:31:08 +0700

    detect search url to form validation

--- api/CustomValidation.py
+++ api/CustomValidation.py
@@ -7,9 +7,9 @@ class CustomValidation(Validation):
             return {'__all__': 'Missing data, please include CompanyName, CompanyId, Contact, Email, and Phone.'}
 
         errors = {}
-        company_id=bundle.data.get('userid', None)
-
+        user_id=bundle.data.get('userid', None)
         # manager method, returns true if the company exists, false otherwise
-        if Member.objects.filter(userid=company_id):
-            errors['userid']='Duplicate CompanyId, CompanyId %s already exists.' % company_id
+        if Member.objects.filter(userid=user_id):
+            errors['error_message']='Duplicate UserID, UserID %s already exists.' % user_id
+            errors['error_code']=1
         return errors
