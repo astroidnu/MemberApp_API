1a585048f45992388d4e232e47091744c7a2f29e
Ibnu Muzzakkir <personalict@gmail.com>
2017-04-24 23:29:34 +0700

    Starting project completing API for method add, delete, get, put

---
+++ api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Member(models.Model):
+    userid= models.CharField(max_length=200, unique=True)
+    fullname = models.CharField(max_length=200)
+    address = models.TextField()
+    dateofbirth = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+        return '%s %s' % (self.fullname, self.address)
---
+++ api/resources.py
@@ -0,0 +1,19 @@
+from tastypie.resources import ModelResource
+from api.models import Member
+from tastypie.authorization import Authorization
+from tastypie.constants import ALL, ALL_WITH_RELATIONS
+
+
+class MemberResources(ModelResource):
+    class Meta:
+        queryset = Member.objects.all()
+        resource_name = 'member'
+        authorization = Authorization()
+        fields = ['id','userid', 'fullname','dateofbirth','address']
+        filtering = {
+            "userid" : ('exact', 'startwith', 'contains'),
+            "userid" : ALL,
+        }
+        allowed_methods = ('get', 'put', 'post', 'delete')
+        include_resource_uri = False
+        always_return_data = True
